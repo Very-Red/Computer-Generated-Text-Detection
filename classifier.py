89563cb48d105e2e1421a64d77e3f72b3c2b3632
Very-Red <bmk5cc@virginia.edu>
2018-03-16 14:36:50 -0400

    classifier

--- classifier.py
+++ classifier.py
@@ -14,19 +14,16 @@ y = []
 for row in X:
     y.append(row[len(row)-1])
 
-#print(y)
 '''
-for line in open("./spambase/spambase.names"):
-    if not line.startswith("|"):
-         item = line.split(':')
-         Y.append(item[0])
+Train dataset
 '''
-
 clf = svm.SVC()
 clf.fit(X[0:4000], y[0:4000])
-
 testX = clf.predict(X[4000:])
 
+'''
+Compare predicted model and actual values.
+'''
 count = 0
 for i in range(len(testX)):
     if testX[i] == y[4000+1]:
