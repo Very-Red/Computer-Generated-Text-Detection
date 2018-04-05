be6107c13944994339491be6cc3c6df118a0dff0
Very-Red <bmk5cc@virginia.edu>
2018-04-05 14:18:23 -0400

    Word counts from fake vs real

--- prob.py
+++ prob.py
@@ -1,17 +1,74 @@
 import sys
 import collections
+import os
 
-def main():
-    '''
-    Read's each line and obtains the words, separated by spaces and
-    places in dictionary (wordcount).
-    Case and punctuation are distinct.
-    '''
-    with open(sys.argv[1],"r") as f:
-        words = collections.Counter(f.read().split())
-    print(words)
+'''
+Read's each line and obtains the words, separated by spaces and
+places in dictionary (wordcount).
+Case and punctuation are distinct. (Should I remove?)
+Also how to stratify? All fake together? Separate by pos/neg? Probably test both.
+'''
 
-    f.close()
 
-if __name__ == '__main__':
-   main()
+'''
+Combines all fake reviews and gets word counts
+'''
+wordsFake = collections.Counter()
+wordsNegFake = collections.Counter()
+wordsPosFake = collections.Counter()
+
+wordsReal = collections.Counter()
+wordsPosReal = collections.Counter()
+wordsNegReal = collections.Counter()
+
+#Negative fake
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/negative_polarity/deceptive_from_MTurk/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            #print(path + filename)
+            for line in f:
+                for word in line.split():
+                    wordsFake[word] += 1
+                    wordsNegFake[word] += 1
+#Positive fake
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/positive_polarity/deceptive_from_MTurk/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            #print(path + filename)
+            for line in f:
+                for word in line.split():
+                    wordsFake[word] += 1
+                    wordsPosFake[word] += 1
+
+#Negative real
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/negative_polarity/truthful_from_Web/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            #print(path + filename)
+            for line in f:
+                for word in line.split():
+                    wordsReal[word] += 1
+                    wordsNegReal[word] += 1
+#Positive fake
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/positive_polarity/truthful_from_TripAdvisor/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            #print(path + filename)
+            for line in f:
+                for word in line.split():
+                    wordsReal[word] += 1
+                    wordsPosReal[word] += 1
+
+
+
+print(collections.Counter(wordsFake).most_common(10))
+print(collections.Counter(wordsNegFake).most_common(10))
+print(collections.Counter(wordsPosFake).most_common(10))
+print(collections.Counter(wordsReal).most_common(10))
+print(collections.Counter(wordsNegReal).most_common(10))
+print(collections.Counter(wordsPosReal).most_common(10))
+f.close()
