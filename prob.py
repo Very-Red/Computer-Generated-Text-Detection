9fea32e9714dd7eba0e79f9a19c6a4d5fd52a169
Very-Red <bmk5cc@virginia.edu>
2018-04-07 20:14:49 -0400

    parts of speech

--- prob.py
+++ prob.py
@@ -2,7 +2,7 @@ import sys
 import collections
 import os
 import string
-
+import nltk
 '''
 Read's each line and obtains the words, separated by spaces and
 places in dictionary (wordcount).
@@ -30,8 +30,11 @@ for i in range (1,6):
             #print(path + filename)
             for line in f:
                 for word in line.split():
-                    wordsFake[word.translate(translator).lower()] += 1
-                    wordsNegFake[word.translate(translator).lower()] += 1
+                    word_token = nltk.word_tokenize(word)
+                    word_tag = nltk.pos_tag(word_token)
+                    if word_tag[0][1] == "JJ" or word_tag[0][1] == "JJR" or word_tag[0][1] == "JJS" or word_tag[0][1] == "NNPS" or word_tag[0][1] == "RB" or word_tag[0][1] == "RBR" or word_tag[0][1] == "RBS" or word_tag[0][1] == "VB" or word_tag[0][1] == "VBD" or word_tag[0][1] == "VBG" or word_tag[0][1] == "VBN" or word_tag[0][1] == "VBZ":
+                        wordsFake[word.translate(translator).lower()] += 1
+                        wordsNegFake[word.translate(translator).lower()] += 1
 #Positive fake
 for i in range (1,6):
     path = "op_spam_v1.4/op_spam_v1.4/positive_polarity/deceptive_from_MTurk/fold" + str(i) + "/"
@@ -40,8 +43,9 @@ for i in range (1,6):
             #print(path + filename)
             for line in f:
                 for word in line.split():
-                    wordsFake[word.translate(translator).lower()] += 1
-                    wordsPosFake[word.translate(translator).lower()] += 1
+                    if word_tag[0][1] == "JJ" or word_tag[0][1] == "JJR" or word_tag[0][1] == "JJS" or word_tag[0][1] == "NNPS" or word_tag[0][1] == "RB" or word_tag[0][1] == "RBR" or word_tag[0][1] == "RBS" or word_tag[0][1] == "VB" or word_tag[0][1] == "VBD" or word_tag[0][1] == "VBG" or word_tag[0][1] == "VBN" or word_tag[0][1] == "VBZ":
+                        wordsFake[word.translate(translator).lower()] += 1
+                        wordsPosFake[word.translate(translator).lower()] += 1
 
 #Negative real
 for i in range (1,6):
@@ -51,8 +55,9 @@ for i in range (1,6):
             #print(path + filename)
             for line in f:
                 for word in line.split():
-                    wordsReal[word.translate(translator).lower()] += 1
-                    wordsNegReal[word.translate(translator).lower()] += 1
+                    if word_tag[0][1] == "JJ" or word_tag[0][1] == "JJR" or word_tag[0][1] == "JJS" or word_tag[0][1] == "NNPS" or word_tag[0][1] == "RB" or word_tag[0][1] == "RBR" or word_tag[0][1] == "RBS" or word_tag[0][1] == "VB" or word_tag[0][1] == "VBD" or word_tag[0][1] == "VBG" or word_tag[0][1] == "VBN" or word_tag[0][1] == "VBZ":
+                        wordsReal[word.translate(translator).lower()] += 1
+                        wordsNegReal[word.translate(translator).lower()] += 1
 #Positive fake
 for i in range (1,6):
     path = "op_spam_v1.4/op_spam_v1.4/positive_polarity/truthful_from_TripAdvisor/fold" + str(i) + "/"
@@ -61,15 +66,21 @@ for i in range (1,6):
             #print(path + filename)
             for line in f:
                 for word in line.split():
-                    wordsReal[word.translate(translator).lower()] += 1
-                    wordsPosReal[word.translate(translator).lower()] += 1
+                    if word_tag[0][1] == "JJ" or word_tag[0][1] == "JJR" or word_tag[0][1] == "JJS" or word_tag[0][1] == "NNPS" or word_tag[0][1] == "RB" or word_tag[0][1] == "RBR" or word_tag[0][1] == "RBS" or word_tag[0][1] == "VB" or word_tag[0][1] == "VBD" or word_tag[0][1] == "VBG" or word_tag[0][1] == "VBN" or word_tag[0][1] == "VBZ":
+                        wordsReal[word.translate(translator).lower()] += 1
+                        wordsPosReal[word.translate(translator).lower()] += 1
 
 
-
+print("Most common words in Fake reviews:")
 print(collections.Counter(wordsFake).most_common(10))
+print("\n Most common words in Negative Fake Reviews")
 print(collections.Counter(wordsNegFake).most_common(10))
+print("\n Most common words in Positive Fake Reviews")
 print(collections.Counter(wordsPosFake).most_common(10))
+print("\n Most common words in Real Reviews")
 print(collections.Counter(wordsReal).most_common(10))
+print("\n Most common words in Negtaive Real Reviews")
 print(collections.Counter(wordsNegReal).most_common(10))
+print("\n Most common words in Positive Real Reviews")
 print(collections.Counter(wordsPosReal).most_common(10))
 f.close()
